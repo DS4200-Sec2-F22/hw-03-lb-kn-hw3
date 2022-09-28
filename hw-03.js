b389c990e91bde11d3ebb790fa803d646b58de3c
Karenna Ng <karenna@MacBook-Air-3.local>
2022-09-28 16:43:08 -0400

    removed inline styling in function

--- hw-03.js
+++ hw-03.js
@@ -1,6 +1,13 @@
-
-
+// shows text and hides button
 function ShowText(elementID, buttonID) {
-	document.getElementById(elementID).style.display="inline";
-	document.getElementById(buttonID).style.display="none";
+
+	// gets text div by Id, replaces hidden display with inline
+	let text = document.getElementById(elementID);
+	text.classList.remove("hide-display");
+	text.classList.add("inline-display");
+
+	// gets button by Id, hides display
+	let button = document.getElementById(buttonID);
+	button.classList.add("hide-display");
+
 }
